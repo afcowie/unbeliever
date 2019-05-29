a09ac6abc5c5a36eba3b4f4bbb75522b96c511db
Andrew Cowie <andrew@operationaldynamics.com>
2019-05-29 17:23:06 +1000

    Handlers and wrapper types around streaming I/O
    Introduce wrappers around streaming I/O. Key idea is that a Handler
    represents a web request's meta + body + response meta + body set along
    with an actually configured (in this case) Snap handler. The key
    function is `handle`, which takes the form
    
        (Request,Input) -> (Response,Output) -> IO (Response,Output)
    
    such that they can be chained together.

--- lib/Core/Http/Server.hs
+++ lib/Core/Http/Server.hs
@@ -2,6 +2,8 @@
 {-# LANGUAGE StrictData #-}
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}        -- FIXME
 {-# OPTIONS_GHC -fno-warn-incomplete-patterns #-}   -- FIXME
 {-# OPTIONS_HADDOCK prune #-}
@@ -17,6 +19,14 @@ serve resources at predictable (and usually trivial) endpoints.
 -}
 module Core.Http.Server
     ( WebService
+    , Handler(..)
+    , Input
+    , readEntire
+    , Output
+    , sendUnto
+    , Inbound(fromInput, intoInput)
+    , Outbound(fromOutput, intoOutput)
+    , handle
     , emptyWebService
     , basicObjectEndpoint
     , launchWebServer
@@ -25,23 +35,136 @@ module Core.Http.Server
 import Control.Concurrent.STM (atomically)
 import Control.Concurrent.STM.TQueue (writeTQueue)
 import Control.Exception (evaluate)
-import qualified Data.ByteString as B
-import Snap.Core (Snap, MonadSnap, Request, getRequest, rqClientAddr
-    , Response, route, writeBS)
+import Control.Monad.State.Class (MonadState(..))
+import Control.Monad.Trans.State (StateT(..))
+import qualified Data.ByteString as B (ByteString)
+import qualified Data.ByteString.Builder as Builder
+import Data.Foldable (foldl')
+import Network.Http.Types (Request, Response)
+import Snap.Core (Snap, MonadSnap, route, writeBS)
+import qualified Snap.Core as Snap (
+      Request, getRequest, rqClientAddr, rqHeaders
+    , Response
+    )
 import Snap.Http.Server (simpleHttpServe, Config, ConfigLog(ConfigNoLog)
     , emptyConfig, setAccessLog, setErrorLog, setBind, setPort, setHostname
     , setDefaultTimeout, setVerbose)
+import System.IO.Streams (InputStream, OutputStream)
+import qualified System.IO.Streams as Streams
 
 import Core.Program.Execute
 import Core.Program.Logging
 import Core.Program.Unlift
 import Core.Text.Rope
+import Core.Text.Bytes
 import Core.System.Base
 
 
-data Handler = Handler {
-    action :: forall m. MonadIO m => Request -> m Response
-}
+data Internal = Internal
+    { requestMeta :: Request
+    , requestBody :: Input
+    , responseMeta :: Response
+    , responseBody :: Output
+    , actualHandler :: Snap ()
+    }
+
+{-|
+An output stream.
+-}
+newtype Input = Input (InputStream B.ByteString) 
+
+{-|
+Reads the /entire/ input. This is often all you need when handling small
+requests, but betwere that this will lead to significant memory pressure on
+large inputs.
+-}
+-- Huh. Y'know? Redoing Bytes as a FingerTree over ShortByteString would be
+-- interesting.
+readEntire :: Input -> IO Bytes
+readEntire (Input i) = do
+    bs <- Streams.toList i
+    let b' = foldl' (\acc b -> acc <> Builder.byteString b) mempty bs
+    return (intoBytes b')
+
+{-|
+An output stream.
+-}
+newtype Output = Output (OutputStream B.ByteString)
+
+{-|
+Send the given data to the output stream.
+
+If you need to send a simple text string the easiest way to do so is using
+'fromRope` on a string literal:
+
+>      sendUnto o (fromRope "That is all")
+
+-}
+sendUnto :: Output -> Bytes -> IO ()
+sendUnto (Output o) b = Streams.write (Just (fromBytes b)) o
+
+{-|
+Adapt a streaming I/O type that can be read from into or from an 'Input'.
+-}
+class Inbound a where
+    intoInput :: a -> Input
+    fromInput :: Input -> a
+
+instance Inbound (Input) where
+    intoInput = id
+    fromInput    = id
+
+instance Inbound (InputStream B.ByteString) where
+    intoInput i = Input i
+    fromInput (Input i) = i
+
+{-|
+Adapt a streaming I/O type that can be written to into an 'Output'.
+-}
+class Outbound a where
+    intoOutput :: a -> Output
+    fromOutput :: Output -> a
+
+instance Outbound (OutputStream B.ByteString) where
+    intoOutput o = Output o
+    fromOutput (Output o) = o
+
+newtype Handler α = Handler (StateT Internal IO α)
+    deriving (Functor, Applicative, Monad, MonadIO, MonadState Internal)
+
+modifyUnderlying :: Snap () -> Handler ()
+modifyUnderlying further = do
+    internal <- get
+    let action = actualHandler internal
+
+    let action' = do
+                    action
+                    further
+
+    put internal { actualHandler = action' }
+
+{-|
+Build a layer of a function to handle a web request. The key idea is that a
+Handler represents a given web request's metadata (headers), body (if any),
+and the building of its response metadata and body. This is made available
+to anyone using this 'handle' function to define a 'Handler'; the signature
+of the function to be passed:
+
+>     layer :: (Request,Input) -> (Response,Output) -> IO (Response,Output)
+
+makes the point that Handlers can be chained together.
+-}
+handle :: ((Request,Input) -> (Response,Output) -> IO (Response,Output)) -> Handler ()
+handle handler = do
+    internal <- get
+
+    let q = requestMeta internal
+        i = requestBody internal
+        p = responseMeta internal
+        o = responseBody internal
+
+    (p',o') <- liftIO (handler (q,i) (p,o))
+    put internal { responseMeta = p', responseBody = o' }
 
 {-|
 A web service. It describes an endpoint which will be handled by the
@@ -59,7 +182,6 @@ instance Semigroup WebService where
 instance Monoid WebService where
     mempty = emptyWebService
     mappend = (<>)
-
 
 emptyWebService :: WebService
 emptyWebService = WebService
